2fcdbce652956ae2ff0ddf925ac095127469e2e1
Vinchenso <chensok@gmail.com>
2016-09-08 23:53:35 +0200

    refactored

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,56 +9,56 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    build_post
   end
 
   def edit
+    load_post
   end
 
   def create
-    @post = Post.new(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
+    build_post(post_params)
+    save_post or render :new
   end
 
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
+    load_post
+    update_post or render :edit
   end
 
   def destroy
+    load_post
     @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to posts_path
   end
 
   private
-    def load_post
-      @post = Post.find(params[:id])
-    end
 
-    def post_params
-      params.require(:post).permit(:name, :title, :content)
-    end
+  def load_post
+    @post = Post.find(params[:id])
+  end
 
-    def load_posts
-      @post ||= Post.all
+  def post_params
+    params.require(:post).permit(:name, :title, :content)
+  end
+
+  def load_posts
+    @posts = Post.all
+  end
+
+  def build_post(arg = nil)
+    @post = Post.new(arg)
+  end
+
+  def save_post
+    if @post.save
+      redirect_to @post
     end
+  end
+
+  def update_post
+    if  @post.update(post_params)
+      redirect_to @post
+    end
+  end
 end
